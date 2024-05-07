f419688a3ca804191c5d7400cd0d92c98aaac505
karciesmagul <karciesmagul@gmail.com>
2024-05-07 13:53:38 +0300

    loosely

--- src/main/java/com/school/loosely/BalanceService.java
+++ src/main/java/com/school/loosely/BalanceService.java
@@ -1,4 +1,24 @@
 package com.school.loosely;
 
+import java.math.BigDecimal;
+
 public class BalanceService {
+
+    public boolean checkout(Balance balance, BigDecimal checkotAmount){
+
+        BigDecimal balanceAmount = balance.getAmount();
+
+
+        // if we have enough money to checkout, proceed and return true, if not return false.
+        // 100-60 = 40
+
+        if(balanceAmount.subtract(checkotAmount).compareTo(BigDecimal.ZERO) >=0){
+
+            //100-60 =40
+            balance.setAmount(balanceAmount.subtract(checkotAmount));
+            return true;
+        }
+        return false;
+    }
+
 }
